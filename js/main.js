5d9fe373cb653d69d15abdfcf0c59a615aabfc7e
xFlqshlight <monapwc2020@gmail.com>
2021-05-07 21:44:42 +0800

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function open_sidebar() { document.getElementById("navbar-2").style.width="250px"; }
 function close_sidebar() { document.getElementById("navbar-2").style.width="0"; }
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-const cb = document.getElementById("opt_color");
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null; // store current theme as setting
+const cb = document.getElementById("opt_color"); // the checkbox
 
 
 // Get current theme setting;
