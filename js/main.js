a5be4c9ec02d373537dc09bbcdf46186a34e80c6
xFlqshlight <monapwc2020@gmail.com>
2021-05-09 20:56:59 +0800

    Added about section

--- js/main.js
+++ js/main.js
@@ -13,11 +13,13 @@ if (currentTheme) {
         document.body.style.setProperty('background-image', 'var(--intro)');
         document.documentElement.style.setProperty('--text-dark', '#6385B5');
         document.documentElement.style.setProperty('--timehover', '#6385B5');
+        document.documentElement.style.setProperty('--fb', '#3b5998'); document.documentElement.style.setProperty('--dc', '#7289da'); document.documentElement.style.setProperty('--ig', '#C13584');
     } else if (currentTheme === 'dark') {
         document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #747474 0%, #656565 50%, #5B5B5B 100%)');
         document.body.style.setProperty('background-image', 'url("assets/bg/bg.jpg")');
         document.documentElement.style.setProperty('--text-dark', '#747474');
         document.documentElement.style.setProperty('--timehover', '#FFFFFF');
+        document.documentElement.style.setProperty('--fb', '#333'); document.documentElement.style.setProperty('--dc', '#333'); document.documentElement.style.setProperty('--ig', '#333');
     }
 }
 
@@ -29,11 +31,13 @@ function opt_color() {
         document.body.style.setProperty('background-image', 'var(--intro)');
         document.documentElement.style.setProperty('--text-dark', '#6385B5');
         document.documentElement.style.setProperty('--timehover', '#6385B5');
+        document.documentElement.style.setProperty('--fb', '#3b5998'); document.documentElement.style.setProperty('--dc', '#7289da'); document.documentElement.style.setProperty('--ig', '#C13584');
     } else {
         localStorage.setItem('theme', 'dark');
         document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #747474 0%, #656565 50%, #5B5B5B 100%)');
         document.body.style.setProperty('background-image', 'url("assets/bg/bg.jpg")');
         document.documentElement.style.setProperty('--text-dark', '#747474');
         document.documentElement.style.setProperty('--timehover', '#FFFFFF');
+        document.documentElement.style.setProperty('--fb', '#333'); document.documentElement.style.setProperty('--dc', '#333'); document.documentElement.style.setProperty('--ig', '#333');
     }
 }
