193debf433dfec8ec525572b73e6490756f70d69
xFlqshlight <monapwc2020@gmail.com>
2021-05-11 21:46:39 +0800

    Preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var preloader = document.getElementById("preload");
+var preloader_text = document.getElementById("preloader-text");
+var preloadtxt = new Array ();
+preloadtxt[0] = "This won't take long.";
+preloadtxt[1] = "Relax while we preload the page.";
+preloadtxt[2] = "Hello.";
+preloadtxt[3] = "Wait a moment while we prepare things.";
+preloadtxt[4] = "Handling your request.";
+
+var i = Math.floor(preloadtxt.length * Math.random());
+preloader_text.innerHTML = preloadtxt[i];
+
+setTimeout(function(){
+    preloader.style.opacity = 0;
+}, 4000); 
+
+setTimeout(function(){
+    preloader.style.display = "none";
+}, 4400); 
+
+
 function open_sidebar() { document.getElementById("navbar-2").style.width="250px"; }
 function close_sidebar() { document.getElementById("navbar-2").style.width="0"; }
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null; // store current theme as setting
@@ -7,37 +28,36 @@ const cb = document.getElementById("opt_color"); // the checkbox
 // Get current theme setting;
 // Load current theme on start
 if (currentTheme) {
-    if (currentTheme === 'color') {
+    if (currentTheme === 'dark') {
         cb.checked = true;
-        document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #c03cff 0%, #784BA0 50%, #2B86C5 100%)');
-        document.body.style.setProperty('background-image', 'var(--intro)');
-        document.documentElement.style.setProperty('--text-dark', '#6385B5');
-        document.documentElement.style.setProperty('--timehover', '#6385B5');
-        document.documentElement.style.setProperty('--fb', '#3b5998'); document.documentElement.style.setProperty('--dc', '#7289da'); document.documentElement.style.setProperty('--ig', '#C13584');
-    } else if (currentTheme === 'dark') {
-        document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #747474 0%, #656565 50%, #5B5B5B 100%)');
-        document.body.style.setProperty('background-image', 'url("assets/bg/bg.jpg")');
-        document.documentElement.style.setProperty('--text-dark', '#747474');
-        document.documentElement.style.setProperty('--timehover', '#FFFFFF');
-        document.documentElement.style.setProperty('--fb', '#333'); document.documentElement.style.setProperty('--dc', '#333'); document.documentElement.style.setProperty('--ig', '#333');
+        document.documentElement.style.setProperty('--light', '#212121');
+        document.documentElement.style.setProperty('--lighter', '#181818');
+        document.documentElement.style.setProperty('--dark', '#EFEFEF');
+        document.documentElement.style.setProperty('--darker', '#FFFFFF');
+    } else if (currentTheme === 'light') {
+        document.documentElement.style.setProperty('--light', '#EFEFEF');
+        document.documentElement.style.setProperty('--lighter', '#FFFFFF');
+        document.documentElement.style.setProperty('--dark', '#212121');
+        document.documentElement.style.setProperty('--darker', '#181818');
     }
 }
 
 // Change theme
 function opt_color() {
     if (cb.checked == true){
-        localStorage.setItem('theme', 'color');
-        document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #c03cff 0%, #784BA0 50%, #2B86C5 100%)');
-        document.body.style.setProperty('background-image', 'var(--intro)');
-        document.documentElement.style.setProperty('--text-dark', '#6385B5');
-        document.documentElement.style.setProperty('--timehover', '#6385B5');
-        document.documentElement.style.setProperty('--fb', '#3b5998'); document.documentElement.style.setProperty('--dc', '#7289da'); document.documentElement.style.setProperty('--ig', '#C13584');
+        localStorage.setItem('theme', 'dark'); //dark mode
+        document.documentElement.style.setProperty('--light', '#212121');
+        document.documentElement.style.setProperty('--lighter', '#181818');
+        document.documentElement.style.setProperty('--dark', '#EFEFEF');
+        document.documentElement.style.setProperty('--darker', '#FFFFFF');
     } else {
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.style.setProperty('--hgradient', 'linear-gradient(315deg, #747474 0%, #656565 50%, #5B5B5B 100%)');
-        document.body.style.setProperty('background-image', 'url("assets/bg/bg.jpg")');
-        document.documentElement.style.setProperty('--text-dark', '#747474');
-        document.documentElement.style.setProperty('--timehover', '#FFFFFF');
-        document.documentElement.style.setProperty('--fb', '#333'); document.documentElement.style.setProperty('--dc', '#333'); document.documentElement.style.setProperty('--ig', '#333');
+        localStorage.setItem('theme', 'light'); //light mode
+        document.documentElement.style.setProperty('--light', '#EFEFEF');
+        document.documentElement.style.setProperty('--lighter', '#FFFFFF');
+        document.documentElement.style.setProperty('--dark', '#212121');
+        document.documentElement.style.setProperty('--darker', '#181818');
     }
 }
+
+//    --dark: #181818;
+// --lightdark: #212121;
