41202dca297a508915600ed4a5760df511f04c96
xFlqshlight <monapwc2020@gmail.com>
2022-07-25 23:24:57 +0800

    decided to edit it cuz im bored

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
-function open_sidebar(){document.getElementById("navbar-2").style.width="250px";}function close_sidebar(){document.getElementById("navbar-2").style.width="0";}
-const currentTheme=localStorage.getItem('theme')?localStorage.getItem('theme'):null;const cb = document.getElementById("opt_color");
-if (currentTheme){if (currentTheme==='dark'){cb.checked = true;document.documentElement.style.setProperty('--light', '#212121');document.documentElement.style.setProperty('--lighter', '#181818');
-document.documentElement.style.setProperty('--dark', '#EFEFEF');document.documentElement.style.setProperty('--darker', '#FFFFFF');
-}else if(currentTheme==='light') {document.documentElement.style.setProperty('--light', '#EFEFEF');document.documentElement.style.setProperty('--lighter', '#FFFFFF');
-document.documentElement.style.setProperty('--dark', '#212121');document.documentElement.style.setProperty('--darker', '#181818');}}
-function opt_color(){if(cb.checked==true){localStorage.setItem('theme', 'dark');document.documentElement.style.setProperty('--light', '#212121');
-document.documentElement.style.setProperty('--lighter', '#181818');document.documentElement.style.setProperty('--dark', '#EFEFEF');document.documentElement.style.setProperty('--darker', '#FFFFFF');
-}else{localStorage.setItem('theme', 'light');document.documentElement.style.setProperty('--light', '#EFEFEF');document.documentElement.style.setProperty('--lighter', '#FFFFFF');
-document.documentElement.style.setProperty('--dark', '#212121');document.documentElement.style.setProperty('--darker', '#181818');}}
-
+function open_sidebar() {
+    document.getElementById("navbar-2").style.width = "250px";
+}
+function close_sidebar() {
+    document.getElementById("navbar-2").style.width = "0";
+}
+const currentTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : null;
+const cb = document.getElementById("opt_color");
+if (currentTheme) {
+    if (currentTheme === "dark") {
+        cb.checked = true;
+        document.documentElement.style.setProperty("--light", "#212121");
+        document.documentElement.style.setProperty("--lighter", "#181818");
+        document.documentElement.style.setProperty("--dark", "#EFEFEF");
+        document.documentElement.style.setProperty("--darker", "#FFFFFF");
+    } else if (currentTheme === "light") {
+        document.documentElement.style.setProperty("--light", "#EFEFEF");
+        document.documentElement.style.setProperty("--lighter", "#FFFFFF");
+        document.documentElement.style.setProperty("--dark", "#212121");
+        document.documentElement.style.setProperty("--darker", "#181818");
+    }
+}
+function opt_color() {
+    if (cb.checked == true) {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.style.setProperty("--light", "#212121");
+        document.documentElement.style.setProperty("--lighter", "#181818");
+        document.documentElement.style.setProperty("--dark", "#EFEFEF");
+        document.documentElement.style.setProperty("--darker", "#FFFFFF");
+    } else {
+        localStorage.setItem("theme", "light");
+        document.documentElement.style.setProperty("--light", "#EFEFEF");
+        document.documentElement.style.setProperty("--lighter", "#FFFFFF");
+        document.documentElement.style.setProperty("--dark", "#212121");
+        document.documentElement.style.setProperty("--darker", "#181818");
+    }
+}
