2299f084bac7e8ff6318d4e3f75d6a97b1540edd
xFlqshlight <monapwc2020@gmail.com>
2022-07-28 16:59:19 +0800

    Added skincare section, update top spotify

--- js/main.js
+++ js/main.js
@@ -85,17 +85,73 @@ function changeSpotify(state) {
         console.log("Selected Last 6 Months Best Songs");
         $("#currentSpotify").text("Last 6 Months");
 
+        $("#song-title-1").text("Valley of Lies");
+        $("#song-artist-1").text("TOMORROW X TOGETHER, iann dior");
+        $("#song-duration-1").text("3:04");
+        $("#song-cover-1").attr("src", "assets/kpop/valleyoflies.jpg");
+
+        $("#song-title-2").text("Life's Too Short (English Version)");
+        $("#song-artist-2").text("aespa");
+        $("#song-duration-2").text("2:58");
+        $("#song-cover-2").attr("src", "assets/kpop/lifetooshort.jpg");
+
+        $("#song-title-3").text("colorblind");
+        $("#song-artist-3").text("Mokita");
+        $("#song-duration-3").text("3:14");
+        $("#song-cover-3").attr("src", "assets/kpop/colorblind.jpg");
+
+        $("#song-title-4").text("Spider");
+        $("#song-artist-4").text("HOSHI");
+        $("#song-duration-4").text("3:32");
+        $("#song-cover-4").attr("src", "assets/kpop/spider.jpg");
+
+        $("#song-title-5").text("LOVE COUNTDOWN (Feat. Wonstein)");
+        $("#song-artist-5").text("NAYEON, Wonstein");
+        $("#song-duration-5").text("3:18");
+        $("#song-cover-5").attr("src", "assets/kpop/imnayeon.jpg");
+
+        $("#song-title-6").text("Carried Away (Love To Love)");
+        $("#song-artist-6").text("Surf Mesa, Madison Beer");
+        $("#song-duration-6").text("3:03");
+        $("#song-cover-6").attr("src", "assets/kpop/carriedaway.jpg");
+
+        $("#song-title-7").text("Future Perfect (Pass The MIC)");
+        $("#song-artist-7").text("ENHYPEN");
+        $("#song-duration-7").text("3:01");
+        $("#song-cover-7").attr("src", "assets/kpop/manifesto.jpg");
+
+        $("#song-title-8").text("Car Crash");
+        $("#song-artist-8").text("eaJ");
+        $("#song-duration-8").text("3:07");
+        $("#song-cover-8").attr("src", "assets/kpop/carcrash.jpg");
+
+        $("#song-title-9").text(
+            "California (Feat. Jackson Wang & Warren Hue) - [Remix]"
+        );
+        $("#song-artist-9").text(
+            "88rising, Rich Brian, NIKI, Jackson Wang, Warren Hue"
+        );
+        $("#song-duration-9").text("4:34");
+        $("#song-cover-9").attr("src", "assets/kpop/california.jpg");
+
+        $("#song-title-10").text("DARARI");
+        $("#song-artist-10").text("TREASURE");
+        $("#song-duration-10").text("3:41");
+        $("#song-cover-10").attr("src", "assets/kpop/dararari.jpg");
+    } else if (state == "LastMonth") {
+        console.log("Selected Last Month Best Songs");
+        $("#currentSpotify").text("Last Month");
         // #1
-        $("#song-title-1").text("Life's Too Short (English Version)");
-        $("#song-artist-1").text("aespa");
-        $("#song-duration-1").text("2:58");
-        $("#song-cover-1").attr("src", "assets/kpop/lifetooshort.jpg");
+        $("#song-title-1").text("Valley of Lies");
+        $("#song-artist-1").text("TOMORROW X TOGETHER, iann dior");
+        $("#song-duration-1").text("3:04");
+        $("#song-cover-1").attr("src", "assets/kpop/valleyoflies.jpg");
 
         // 2
-        $("#song-title-2").text("colorblind");
-        $("#song-artist-2").text("Mokita");
-        $("#song-duration-2").text("3:14");
-        $("#song-cover-2").attr("src", "assets/kpop/colorblind.jpg");
+        $("#song-title-2").text("Life's Too Short (English Version)");
+        $("#song-artist-2").text("aespa");
+        $("#song-duration-2").text("2:58");
+        $("#song-cover-2").attr("src", "assets/kpop/lifetooshort.jpg");
 
         // 3
         $("#song-title-3").text("Spider");
@@ -110,106 +166,40 @@ function changeSpotify(state) {
         $("#song-cover-4").attr("src", "assets/kpop/imnayeon.jpg");
 
         // 5
-        $("#song-title-5").text("Carried Away (Love To Love)");
-        $("#song-artist-5").text("Surf Mesa, Madison Beer");
-        $("#song-duration-5").text("3:03");
-        $("#song-cover-5").attr("src", "assets/kpop/carriedaway.jpg");
-
-        // 6
-        $("#song-title-6").text("Future Perfect (Pass The MIC)");
-        $("#song-artist-6").text("ENHYPEN");
-        $("#song-duration-6").text("3:01");
-        $("#song-cover-6").attr("src", "assets/kpop/manifesto.jpg");
-
-        // 7
-        $("#song-title-7").text("Car Crash");
-        $("#song-artist-7").text("eaJ");
-        $("#song-duration-7").text("3:07");
-        $("#song-cover-7").attr("src", "assets/kpop/carcrash.jpg");
-
-        // 8
-        $("#song-title-8").text(
-            "California (Feat. Jackson Wang & Warren Hue) - [Remix]"
-        );
-        $("#song-artist-8").text(
-            "88rising, Rich Brian, NIKI, Jackson Wang, Warren Hue"
-        );
-        $("#song-duration-8").text("4:34");
-        $("#song-cover-8").attr("src", "assets/kpop/california.jpg");
-
-        // 9
-        $("#song-title-9").text("DARARI");
-        $("#song-artist-9").text("TREASURE");
-        $("#song-duration-9").text("3:41");
-        $("#song-cover-9").attr("src", "assets/kpop/dararari.jpg");
-
-        // 10
-        $("#song-title-10").text("Here's Your Perfect (with salem ilese)");
-        $("#song-artist-10").text("Jamie Miller, salem ilese");
-        $("#song-duration-10").text("2:39");
-        $("#song-cover-10").attr("src", "assets/kpop/hereyourperfect.jpg");
-    } else if (state == "LastMonth") {
-        console.log("Selected Last Month Best Songs");
-        $("#currentSpotify").text("Last Month");
-        // #1
-        $("#song-title-1").text("Life's Too Short (English Version)");
-        $("#song-artist-1").text("aespa");
-        $("#song-duration-1").text("2:58");
-        $("#song-cover-1").attr("src", "assets/kpop/lifetooshort.jpg");
-
-        // 2
-        $("#song-title-2").text("Spider");
-        $("#song-artist-2").text("HOSHI");
-        $("#song-duration-2").text("3:32");
-        $("#song-cover-2").attr("src", "assets/kpop/spider.jpg");
-
-        // 3
-        $("#song-title-3").text("LOVE COUNTDOWN (Feat. Wonstein)");
-        $("#song-artist-3").text("NAYEON, Wonstein");
-        $("#song-duration-3").text("3:18");
-        $("#song-cover-3").attr("src", "assets/kpop/imnayeon.jpg");
-
-        // 4
-        $("#song-title-4").text("Future Perfect (Pass The MIC)");
-        $("#song-artist-4").text("ENHYPEN");
-        $("#song-duration-4").text("3:01");
-        $("#song-cover-4").attr("src", "assets/kpop/manifesto.jpg");
-
-        // 5
-        $("#song-title-5").text("TFW (That Feeling When)");
+        $("#song-title-5").text("Future Perfect (Pass The MIC)");
         $("#song-artist-5").text("ENHYPEN");
-        $("#song-duration-5").text("3:18");
+        $("#song-duration-5").text("3:01");
         $("#song-cover-5").attr("src", "assets/kpop/manifesto.jpg");
 
         // 6
-        $("#song-title-6").text("your love");
-        $("#song-artist-6").text("brb., Jimmy Brown");
-        $("#song-duration-6").text("2:36");
-        $("#song-cover-6").attr("src", "assets/kpop/yourlove.jpg");
+        $("#song-title-6").text("TFW (That Feeling When)");
+        $("#song-artist-6").text("ENHYPEN");
+        $("#song-duration-6").text("3:18");
+        $("#song-cover-6").attr("src", "assets/kpop/manifesto.jpg");
 
         // 7
-        $("#song-title-7").text("Sour Grapes");
-        $("#song-artist-7").text("LE SSERAFIM");
-        $("#song-duration-7").text("3:17");
-        $("#song-cover-7").attr("src", "assets/kpop/serafim.jpg");
+        $("#song-title-7").text("your love");
+        $("#song-artist-7").text("brb., Jimmy Brown");
+        $("#song-duration-7").text("2:36");
+        $("#song-cover-7").attr("src", "assets/kpop/yourlove.jpg");
 
         // 8
-        $("#song-title-8").text("LIMBO");
-        $("#song-artist-8").text("keshi");
-        $("#song-duration-8").text("3:33");
-        $("#song-cover-8").attr("src", "assets/kpop/limbo.jpg");
+        $("#song-title-8").text("Sour Grapes");
+        $("#song-artist-8").text("LE SSERAFIM");
+        $("#song-duration-8").text("3:17");
+        $("#song-cover-8").attr("src", "assets/kpop/serafim.jpg");
 
         // 9
-        $("#song-title-9").text("Betty (Get Money)");
-        $("#song-artist-9").text("Yung Gravy");
-        $("#song-duration-9").text("2:26");
-        $("#song-cover-9").attr("src", "assets/kpop/betty.jpg");
+        $("#song-title-9").text("LIMBO");
+        $("#song-artist-9").text("keshi");
+        $("#song-duration-9").text("3:33");
+        $("#song-cover-9").attr("src", "assets/kpop/limbo.jpg");
 
         // 10
-        $("#song-title-10").text("Golden Hour");
-        $("#song-artist-10").text("HRVY");
-        $("#song-duration-10").text("2:38");
-        $("#song-cover-10").attr("src", "assets/kpop/goldenhour.jpg");
+        $("#song-title-10").text("Cat & Dog");
+        $("#song-artist-10").text("TOMORROW X TOGETHER");
+        $("#song-duration-10").text("3:07");
+        $("#song-cover-10").attr("src", "assets/kpop/catdog.jpg");
     } else {
         console.log("No state selected");
     }
